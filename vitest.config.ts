464275656b9bf04f8667b93c19dd845b3aa9d29c
Luka <salevic@lu-ka.me>
2023-08-03 22:00:12 +0200

    Fix `vitest` config to detect path aliases properly via `jsdom`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,14 @@
-import path from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
-
-const resolvedPath = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   resolve: {
-    alias: {
-      "~/*": `${resolvedPath}/*`,
-    }
+    alias: [{ find: "~", replacement: resolve(__dirname, "./src") }]
   }
 });
