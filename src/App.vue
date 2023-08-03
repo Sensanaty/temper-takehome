cba39d708c7daf67253245e8fd6db9e84a907a65
Luka <salevic@lu-ka.me>
2023-08-03 15:25:17 +0200

    Create the Slanted Banner as a component imported in the main `App.vue` file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
+  <SlantedBanner />
+
   <div class="h-full">
     <h1 class="font-black text-accent text-5xl text-center">
       Hello, Temper!
     </h1>
   </div>
 </template>
+
+<script setup lang="ts">
+  import SlantedBanner from "~/components/Miscellaneous/SlantedBanner.vue";
+</script>
---
+++ src/components/Miscellaneous/SlantedBanner.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="absolute bg-accent-900 w-full h-screen shadow-md skew-y-[350deg] -top-[85%] -z-10" aria-hidden="true" />
+</template>
