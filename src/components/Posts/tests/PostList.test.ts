9dfe696f846a7d5e68b4e9bc97acf6eeaa7417ca
Luka <salevic@lu-ka.me>
2023-08-04 11:39:45 +0200

    Add a comment to the PostList explaining an issue with the Suspense component

--- src/components/Posts/tests/PostList.test.ts
+++ src/components/Posts/tests/PostList.test.ts
@@ -2,6 +2,10 @@ import { render } from "@testing-library/vue";
 import { describe, it, expect } from "vitest";
 import PostList from "~/components/Posts/PostList.vue";
 
+// The <Suspense> Component seems to be causing some weird issues with trying to test this component,
+// it gives me very vague warnings that I can't really figure out how to fix, mostly spitting out some vitest internals.
+// Given more time I could probably figure it out, but as is I'm not sure what the solution would be here
+
 describe("PostList", () => {
   it.skip("should render", async () => {
     const wrapper = render(PostList);
